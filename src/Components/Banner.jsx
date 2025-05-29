67fee7f8743899c5da00c8a229cd922376cfb39b
Deepak Chauhan <dchauhan2159@gmail.com>
2025-05-29 20:16:45 +0530

    Code Added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -18,11 +18,12 @@ function Banner() {
   }, []);
 
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-center px-6 md:px-16 py-10 min-h-screen  text-white">
-      
+    <div className="flex flex-col-reverse md:flex-row items-center px-8 justify-center md:px-16  min-h-screen  text-white">
       {/* Left Content */}
-      <div className="w-full md:w-1/2 flex flex-col items-start md:items-start justify-center text-center md:text-left mb-10 md:mb-0">
-        <h2 className="font-bold text-3xl md:text-4xl mb-4">Hello! I'm Deepak Chauhan</h2>
+      <div className="w-full md:w-1/2 flex flex-col items-start md:items-start justify-center text-center md:text-left mb-30 md:mb-0">
+        <h2 className="font-bold text-3xl md:text-4xl mb-4">
+          Hello! I'm Deepak Chauhan
+        </h2>
         <p className="font-semibold text-xl md:text-2xl">
           I like{" "}
           <span className="text-slate-900 bg-white px-3 py-1 rounded-2xl transition-all duration-300">
@@ -30,14 +31,35 @@ function Banner() {
           </span>
         </p>
         <div className="flex gap-6 text-3xl mt-8 justify-center md:justify-start">
-          <a href="https://www.linkedin.com/in/deepak-chauhan-69426622b/" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} className="hover:text-blue-500 transition" />
+          <a
+            href="https://www.linkedin.com/in/deepak-chauhan-69426622b/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="hover:text-blue-500 transition"
+            />
           </a>
-          <a href="https://github.com/Code-walah-07" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} className="hover:text-gray-400 transition" />
+          <a
+            href="https://github.com/Code-walah-07"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="hover:text-gray-400 transition"
+            />
           </a>
-          <a href="https://www.hackerrank.com/profile/dchauhan2159" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faHackerrank} className="hover:text-green-500 transition" />
+          <a
+            href="https://www.hackerrank.com/profile/dchauhan2159"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faHackerrank}
+              className="hover:text-green-500 transition"
+            />
           </a>
         </div>
       </div>
